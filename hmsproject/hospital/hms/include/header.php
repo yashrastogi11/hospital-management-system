0ae3a337377fee51af8ce9b432f86a74d36c9547
Yash Rastogi <yashrastogi11@gmail.com>
2019-02-23 18:06:00 +0530

    First commit

---
+++ hmsproject/hospital/hms/include/header.php
@@ -0,0 +1,51 @@
+<?php error_reporting(0);?>
+<header class="navbar navbar-default navbar-static-top">
+					<!-- start: NAVBAR HEADER -->
+					
+					<!-- end: NAVBAR HEADER -->
+					<!-- start: NAVBAR COLLAPSE -->
+					<div class="navbar-collapse collapse">
+						<ul class="nav navbar-right">
+							<!-- start: MESSAGES DROPDOWN -->
+								<li  style="padding-top:2% ">
+								<h2>Hospital Management System</h2>
+							</li>
+						
+						
+							<li class="dropdown current-user">
+								<a href class="dropdown-toggle" data-toggle="dropdown">
+									 <span class="username">
+
+
+
+									<?php $query=mysqli_query($con,"select fullName from users where id='".$_SESSION['id']."'");
+while($row=mysqli_fetch_array($query))
+{
+	echo $row['fullName'];
+}
+									?> <i class="ti-angle-down"></i></i></span>
+								</a>
+								<ul class="dropdown-menu dropdown-dark">
+									
+								
+									
+									<li>
+										<a href="logout.php">
+											Log Out
+										</a>
+									</li>
+								</ul>
+							</li>
+							<!-- end: USER OPTIONS DROPDOWN -->
+						</ul>
+						<!-- start: MENU TOGGLER FOR MOBILE DEVICES -->
+						<div class="close-handle visible-xs-block menu-toggler" data-toggle="collapse" href=".navbar-collapse">
+							<div class="arrow-left"></div>
+							<div class="arrow-right"></div>
+						</div>
+						<!-- end: MENU TOGGLER FOR MOBILE DEVICES -->
+					</div>
+				
+					
+					<!-- end: NAVBAR COLLAPSE -->
+				</header>
